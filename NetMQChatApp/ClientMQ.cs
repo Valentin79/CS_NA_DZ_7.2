fed43fbec1ccf6c44b71f4fe8947774a25cd45ea
Valentin <99755643+Valentin79@users.noreply.github.com>
2023-12-30 16:00:20 +0300

    dz7

---
+++ NetMQChatApp/ClientMQ.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace NetMQChatApp
+{
+    public class ClientMQ
+    {
+        public static void SendMessage(string msg)
+        {
+            using (var client = new RequestSocket())
+            {
+                //client.Connect("tcp://localhost:5555");
+                client.Connect("tcp://127.0.0.1:5556");
+                client.SendFrame(msg);
+                var message = client.ReceiveFrameString();
+                Console.WriteLine("Received: ", message);
+                
+            }
+        }
+
+    }
+}
---
+++ NetMQChatApp/ServerMQ.cs
@@ -0,0 +1,26 @@
+﻿
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace NetMQChatApp
+{
+    public class ServerMQ
+    {
+        public static void Server()
+        {
+            
+            using (var server = new ResponseSocket())
+            {
+                //server.Bind("tcp://*:5555");
+                server.Bind("tcp://127.0.0.1:5556");
+                Console.WriteLine("Запускаю сервер");
+                while (true)
+                {
+                    var message = server.ReceiveFrameString();
+                    Console.WriteLine("Received:", message);
+                    server.SendFrame("Сообщение получено");
+                }
+            }
+        }
+    }
+}
