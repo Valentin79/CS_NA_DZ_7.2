e23275d69f7af8f10e32f4849abb0383980cc092
Valentin <99755643+Valentin79@users.noreply.github.com>
2023-12-30 16:02:35 +0300

    dz7

---
+++ SolutionMQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using NetMQChatApp;
+namespace SolutionMQ
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                ServerMQ.Server();
+            }
+            else
+            {
+                ClientMQ.SendMessage(args[0]);
+            }
+        }
+    }
+}
